9b2e87c0972f1c8fb6a521ce2993153f095d756d
Haeun Noh <00blowup@gmail.com>
2022-11-13 23:02:56 +0900

    Flask에 맞게 프로젝트 폴더 구조 정리

---
+++ application.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+
+@app.route("/addmenu")
+def addmenu():
+    return render_template("AddMenu.html")
+
+
+
+app.run(debug=True)
