66aa7f3c0003b46ac88a948a18de9ee30e9c1ae3
Sunho12 <113418806+Sunho12@users.noreply.github.com>
2022-11-15 17:39:02 +0900

    Update database.py

--- database.py
+++ database.py
@@ -8,4 +8,18 @@ class DBhandler:
         firebase = pyrebase.initialize_app(config)
         self.db = firebase.database()
 
-        
+#RegisterPage
+    def insert_restaurant(self,name,data,img_path):
+    restaurant_info={
+      "name":data['name'],
+      "type":data['type'],
+      "location":data['location'],
+      "locatedetail":data['locationdetail'],
+      "phone":data['phone'],
+      "mon":{data['closed'],data['start'],data['end'],data['bstart'],data['bend']},
+      "extra":data['extra'],
+      "img_path":img_path
+    }
+    self.db.child("restaurant").child(name).set(restaurant_info)
+    print(data,img_path)
+    return True
