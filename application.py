c976daf354b525016a718e7e7d8e8577ea3b6b81
rwaeng <80890092+rwaeng@users.noreply.github.com>
2022-11-19 22:44:25 +0900

    Update application.py

--- application.py
+++ application.py
@@ -59,6 +59,10 @@ def viewreview():
 def writereview():
     return render_template("WriteReview.html")
 
+@application.route("/mypage")
+def mypage():
+    return render_template("MyPage.html")
+
 
 #WriteReview
 @application.route("/submit_review", methods=['POST'])
@@ -74,17 +78,11 @@ def reg_review_submit():
 #RegisterPage
 @application.route("/submit_register",methods=['POST'])
 def reg_register_submit():
-    if request.method == 'POST' :
-        image_file=request.files["register_img"]
-        image_file.save("static/uploads/{}". format(image_file.filename))
-        data = request.form
-        print(image_file, data.get("name"), data.get("type"), data.get("location"), data.get("locationdetail"), data.get("phone"), data.get("day"), data.get("start"), data.get("end"), data.get("bstart"), data.get("bend"), data.get("extra"))
-
-    if DB.insert_restaurant(data['name'],data,image_file.filename):
-        return render_template("RegisterPage_result.html",data=data,img_path="static/uploads/" + image_file.filename)
-    else:
-        return "Restaurant name already exist!"
-
+    image_file=request.files["file"]
+    image_file.save("static/uploads/register_image.png")
+    data = request.form
+    print(image_file, data.get("name"), data.get("type"), data.get("location"), data.get("locationdetail"), data.get("phone"), data.get("day"), data.get("start1"), data.get("start2"), data.get("start3"), data.get("start4"), data.get("start5"), data.get("start6"), data.get("start7"), data.get("end1"),  data.get("end2"), data.get("end3"),  data.get("end4"),  data.get("end5"),  data.get("end6"), data.get("end7"), data.get("bstart"), data.get("bend"), data.get("extra"))
+    return render_template("RegisterPage_result.html",data=data)
 
 #AddMenu
 @application.route("/submit_menu", methods=['POST'])
