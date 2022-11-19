881ab1a069ca9119d866931ffbfa50f60b46281c
Sunho12 <113418806+Sunho12@users.noreply.github.com>
2022-11-19 20:32:44 +0900

    Update database.py

--- database.py
+++ database.py
@@ -8,7 +8,8 @@ class DBhandler:
         firebase = pyrebase.initialize_app(config)
         self.db = firebase.database()
 
-    #RegisterPage
+        
+   #RegisterPage
     def insert_restaurant(self,name,data,img_path):
         restaurant_info={
         "name":data['name'],
@@ -20,13 +21,20 @@ class DBhandler:
         "extra":data['extra'],
         "img_path":img_path
         }
-        self.db.child("restaurant").child(name).set(restaurant_info)
-        print(data,img_path)
-        return True
-    
-     def restaurant_duplicate_check(self, name):
+
+        if self.restaurant_duplicate_check(name):
+          self.db.child("restaurant").child(name).set(restaurant_info)
+          print(data,img_path)
+          return True
+        else:
+          return False
+
+
+
+    def restaurant_duplicate_check(self, name):
         restaurants = self.db.child("restaurant").get()
         for res in restaurants.each():
           if res.key()==name:
             return False
         return True
+
